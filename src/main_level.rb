d42d8e8c53ba73dd0677e9f708fc9526b00e8e28
John Croisant <jacius@gmail.com>
2009-06-26 18:14:27 -0500

    MainLevel creates pivot correctly.

--- src/main_level.rb
+++ src/main_level.rb
@@ -12,9 +12,11 @@ class MainLevel < Level
     c = [240, 240]
     r = 206
 
-    create_actor :pivot, :x => c[0], :y => c[1], :radius => r
+    @pivot = create_actor :pivot_actor, :x => c[0], :y => c[1], :radius => r
 
   end
+
+  attr_reader :pivot
 
   def draw(target, x_off, y_off)
     @background.blit(target.screen,[0,0])
