4f35421d15a618acb9fae44dcf0bfa488fdd0247
John Croisant <jacius@gmail.com>
2009-06-26 18:22:07 -0500

    MainLevel loads center and radius from opts.

--- src/main_level.rb
+++ src/main_level.rb
@@ -5,12 +5,8 @@ class MainLevel < Level
   def setup
     @background = @resource_manager.load_image('background.png')
 
-    # Supposed to be able to do this, but can't?
-    # c = opts[:center]
-    # r = opts[:radius]
-
-    c = [240, 240]
-    r = 206
+    c = opts[:center]
+    r = opts[:radius]
 
     @pivot = create_actor :pivot_actor, :x => c[0], :y => c[1], :radius => r
 
