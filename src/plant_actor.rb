b4745a57cffe8de074727eea80dcb613b66b52ad
John Croisant <jacius@gmail.com>
2009-06-26 16:19:14 -0500

    Added "plant_actor.rb"

---
+++ src/plant_actor.rb
@@ -0,0 +1,18 @@
+require 'actor'
+require 'actor_view'
+
+
+class PlantActorView < ActorView
+
+  def draw(target, x_off, y_off)
+  end
+
+end
+
+
+class PlantActor < Actor
+
+  def setup
+  end
+
+end
