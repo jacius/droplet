d14ce96a5ec42e13fd950ef723e0630bec0a5ba0
John Croisant <jacius@gmail.com>
2009-06-26 18:07:49 -0500

    MainLevel creates a pivot actor. Or tries, anyway.

--- src/main_level.rb
+++ src/main_level.rb
@@ -1,9 +1,19 @@
 require 'level'
-require 'ftor'
+require 'vector2'
 
 class MainLevel < Level
   def setup
     @background = @resource_manager.load_image('background.png')
+
+    # Supposed to be able to do this, but can't?
+    # c = opts[:center]
+    # r = opts[:radius]
+
+    c = [240, 240]
+    r = 206
+
+    create_actor :pivot, :x => c[0], :y => c[1], :radius => r
+
   end
 
   def draw(target, x_off, y_off)
