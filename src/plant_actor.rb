68a6f4a9f70dd58a1de537e2764054f92d62dba2
John Croisant <jacius@gmail.com>
2009-06-27 02:59:32 -0500

    Updated PlantActor a lot.

--- src/plant_actor.rb
+++ src/plant_actor.rb
@@ -1,10 +1,15 @@
 require 'actor'
 require 'actor_view'
 
+require 'vector2'
+require 'plant_types'
+
 
 class PlantActorView < ActorView
 
   def draw(target, x_off, y_off)
+    offset = Vector2.new(x_off, y_off)
+    actor.root.draw( target, actor.pos+offset, actor.angle, 1.0 )
   end
 
 end
@@ -13,6 +18,17 @@ end
 class PlantActor < Actor
 
   def setup
+    @pos = Vector2.new( @opts[:x], @opts[:y] )
+    @angle = @level.pivot.pos2angle(@pos) + Math::PI
+    @type = @opts[:type]
+    @root = @type.first_node
+  end
+
+  attr_reader :pos, :angle, :type, :root
+
+
+  def tick
+    @root.tick
   end
 
 end
