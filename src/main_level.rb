9fd2481f8e9d30b2047ad580fd1728601988f5b0
John Croisant <jacius@gmail.com>
2009-06-28 09:53:36 -0500

    Refactored code to make MainLevel#create_random_plant.

--- src/main_level.rb
+++ src/main_level.rb
@@ -18,26 +18,7 @@ class MainLevel < Level
 
 
     @pivot.input_manager.reg MouseDownEvent, :left do |event|
-      p = @pivot.nearest_point( event.pos )
-
-      hue1 = rand
-      hue2 = (hue1 + 0.2)%1.0
-      val1 = 0.3 + rand*0.3
-      val2 = val1 - 0.2
-      long = 35 + rand*10
-
-      i = rand(3)
-      gens = [3,4,5][i]
-      maxchilds = [4,3,2][i]
-
-      opts = { 
-        :hue1 => hue1, :val1 => val1,
-        :hue2 => hue2, :val2 => val2,
-        :maxlong => long,
-        :maxchilds => maxchilds,
-        :gens => gens }
-
-      create_plant( SamplePlantType.new(opts), :x => p.x, :y => p.y )
+      create_random_plant( event )
     end
 
   end
@@ -51,6 +32,30 @@ class MainLevel < Level
 
   def update( time )
     @plants.each { |plant| plant.grow( time * 0.001 ) }
+  end
+
+
+  def create_random_plant( event )
+    p = @pivot.nearest_point( event.pos )
+
+    hue1 = rand
+    hue2 = (hue1 + 0.2)%1.0
+    val1 = 0.3 + rand*0.3
+    val2 = val1 - 0.2
+    long = 35 + rand*10
+
+    i = rand(3)
+    gens = [3,4,5][i]
+    maxchilds = [4,3,2][i]
+
+    opts = { 
+      :hue1 => hue1, :val1 => val1,
+      :hue2 => hue2, :val2 => val2,
+      :maxlong => long,
+      :maxchilds => maxchilds,
+      :gens => gens }
+
+    create_plant( SamplePlantType.new(opts), :x => p.x, :y => p.y )
   end
 
 
